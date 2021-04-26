0d03995ad48cc59da0d79eab8f5633b448aee777
Hanna <ha7008jo-s@student.lu.se>
2021-04-26 17:49:32 +0200

    Gave MoveList an ArrayList for moves.

--- app/src/main/java/com/example/venux/MoveList.java
+++ app/src/main/java/com/example/venux/MoveList.java
@@ -1,4 +1,12 @@
 package com.example.venux;
 
+import java.util.ArrayList;
+
 public class MoveList {
+
+    private ArrayList<Move> moves;
+
+    public MoveList(){
+        this.moves = new ArrayList<Move>();
+    }
 }
