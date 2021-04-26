2b19fbd66d457dd3b0cd2394698eee9975337ead
Hanna <ha7008jo-s@student.lu.se>
2021-04-26 17:53:35 +0200

    gave Game their attributes

--- app/src/main/java/com/example/venux/Game.java
+++ app/src/main/java/com/example/venux/Game.java
@@ -1,4 +1,21 @@
 package com.example.venux;
 
+import java.util.ArrayList;
+
 public class Game {
+
+    private ArrayList<Player> players;
+    private Player currentPlayer;
+    private MoveList moveList;
+    private Move currentMove;
+    private int numberOfRounds;
+    private int currentRound;
+
+    public Game(){
+        this.players = new ArrayList<Player>();
+        this.moveList = new MoveList();
+        this.numberOfRounds=0;
+        this.currentRound=0;
+    }
+
 }
