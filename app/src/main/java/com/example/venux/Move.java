75e8a3b8819304725886125b093c50c109f36cb5
Hanna <ha7008jo-s@student.lu.se>
2021-04-26 17:36:27 +0200

    Filled Move upp with attributes.

--- app/src/main/java/com/example/venux/Move.java
+++ app/src/main/java/com/example/venux/Move.java
@@ -1,4 +1,22 @@
 package com.example.venux;
 
 public class Move {
+
+    private float[] xAcc;
+    private float[] yAcc;
+    private float[] zAcc;
+    private float[] xRotation;
+    private float[] yRotation;
+    private float[] zRotation;
+
+    public Move(){
+        this.xAcc = new float[1];
+        this.yAcc = new float[1];
+        this.zAcc = new float[1];
+        this.xRotation = new float[1];
+        this.yRotation = new float[1];
+        this.zRotation = new float[1];
+
+    }
+
 }
