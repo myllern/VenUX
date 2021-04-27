a7358d61a6accf6e43e48de8e65f6514a894b979
Hanna <ha7008jo-s@student.lu.se>
2021-04-27 14:16:16 +0200

    implemented simple move comparator and tested

--- app/src/test/java/com/example/venux/MoveTest.java
+++ app/src/test/java/com/example/venux/MoveTest.java
@@ -1,17 +1,58 @@
 package com.example.venux;
 
+import android.util.Log;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.Console;
 
 import static org.junit.Assert.*;
 
 public class MoveTest {
 
+    Move move1;
+    Move move2;
+
     @Before
     public void setUp() throws Exception {
+        move1 = new Move();
+        move2 = new Move();
     }
 
     @After
     public void tearDown() throws Exception {
+        move1 = null;
+        move2 = null;
+    }
+
+    @Test
+    public void testSimpleMoveComparator(){
+
+        move1.getxRotation()[0]=3;
+        move1.getxRotation()[0]=3;
+        move1.getxRotation()[0]=3;
+
+        move2.getxRotation()[0]=3;
+        move2.getxRotation()[0]=3;
+        move2.getxRotation()[0]=-1;
+        assertFalse(move1.isSimpleMoveCloseEnough(move2));
+
+    }
+
+    @Test
+    public void testSimpleMoveComparatorSuccess(){
+
+        move1.getxRotation()[0]=3;
+        move1.getxRotation()[0]=3;
+        move1.getxRotation()[0]=3;
+
+        move2.getxRotation()[0]=3;
+        move2.getxRotation()[0]=3;
+        move2.getxRotation()[0]=0;
+        assertTrue(move1.isSimpleMoveCloseEnough(move2));
+
     }
 }
