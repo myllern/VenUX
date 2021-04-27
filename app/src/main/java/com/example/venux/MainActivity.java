fa7e6b53c8d65acae5c6ceb18de9c26a9a159b9c
iDaviddd <david@ohrbom.se>
2021-04-27 14:57:48 +0200

    Added start view and player setup view

--- app/src/main/java/com/example/venux/MainActivity.java
+++ app/src/main/java/com/example/venux/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.venux;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.venux.Controller.*;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    PlayerSetup playerSetup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Testish
 
+    }
+
+    public void navToPlayerSetup(View view) {
+        Intent playerSetup = new Intent(this, PlayerSetup.class);
+        startActivity(playerSetup);
     }
 }
--- app/src/main/java/com/example/venux/PlayerSetup.java
+++ app/src/main/java/com/example/venux/PlayerSetup.java
@@ -3,6 +3,7 @@ package com.example.venux;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class PlayerSetup extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class PlayerSetup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_setup);
+    }
+
+    public void startGame(View view) {
+        System.out.println("Start game now!");
     }
 }
