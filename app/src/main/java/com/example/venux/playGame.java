75a0678cacf6f5f2d9d82395d22b8633d58d0fcc
myllern <49235951+myllern@users.noreply.github.com>
2021-04-28 08:45:56 +0200

    playGame

---
+++ app/src/main/java/com/example/venux/playGame.java
@@ -0,0 +1,14 @@
+package com.example.venux;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class playGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_game);
+    }
+}
