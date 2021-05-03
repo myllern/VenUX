423c097309213172477a05bf2ed54345bdcc232b
adlajebara <adlajebara@gmail.com>
2021-05-03 15:28:07 +0200

    Changed countdown from 10 to 5

--- app/src/main/java/com/example/venux/Timer.java
+++ app/src/main/java/com/example/venux/Timer.java
@@ -5,7 +5,7 @@ public class Timer implements Runnable {
     @Override
     public void run() {
         long t0 = System.currentTimeMillis();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             long now = System.currentTimeMillis();
             System.out.println(now - t0);
             long d = (t0 +(i+1)*1000) - now;
